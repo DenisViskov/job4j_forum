21a1751e352853d8fd2992cee4463d1bd8d15f89
DenisViskov <denisviskov16@gmail.com>
2020-10-28 00:38:12 +0300

    update

--- src/main/java/forum/model/User.java
+++ src/main/java/forum/model/User.java
@@ -1,7 +1,13 @@
 package forum.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * @author Денис Висков
@@ -10,7 +16,7 @@ import java.util.Objects;
  */
 @Entity
 @Table(name = "usr")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -18,6 +24,8 @@ public class User {
     private String name;
     @Column(name = "password")
     private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
 
     public User() {
     }
@@ -44,12 +52,51 @@ public class User {
         this.name = name;
     }
 
+    @Override
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public String getUsername() {
+        return getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     @Override
@@ -59,11 +106,12 @@ public class User {
         User user = (User) o;
         return id == user.id &&
                 Objects.equals(name, user.name) &&
-                Objects.equals(password, user.password);
+                Objects.equals(password, user.password) &&
+                Objects.equals(roles, user.roles);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, password);
+        return Objects.hash(id, name, password, roles);
     }
 }
--- src/main/java/forum/service/UserService.java
+++ src/main/java/forum/service/UserService.java
@@ -1,13 +1,20 @@
 package forum.service;
 
+import forum.model.Role;
 import forum.model.User;
+import forum.persistance.RoleRepository;
 import forum.persistance.Store;
 import forum.persistance.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,16 +24,22 @@ import java.util.Optional;
  * @since 26.10.2020
  */
 @Service
-public class UserService implements RepositoryService<User> {
+public class UserService implements RepositoryService<User>, UserDetailsService {
     private final UserRepository store;
+    private final RoleRepository roleStore;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserService(UserRepository store) {
+    public UserService(UserRepository store, RoleRepository roleStore, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.store = store;
+        this.roleStore = roleStore;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
     public User add(User some) {
+        some.setRoles(Collections.singleton(new Role(1, "ROLE_USER")));
+        some.setPassword(bCryptPasswordEncoder.encode(some.getPassword()));
         return (User) store.save(some);
     }
 
@@ -44,6 +57,12 @@ public class UserService implements RepositoryService<User> {
 
     @Override
     public void update(User some) {
+        if(store.findById(some.getId()).isPresent())
         store.save(some);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
     }
 }
